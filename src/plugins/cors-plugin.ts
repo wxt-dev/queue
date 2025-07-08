c2b92d58882ef52893c9c913cdbe5dc3788efc99
Aaron <aaronklinker1@gmail.com>
2025-07-08 02:46:02 -0500

    fix: Accept CORS requests correctly

--- src/plugins/cors-plugin.ts
+++ src/plugins/cors-plugin.ts
@@ -1,12 +1,13 @@
 import { createApp } from "@aklinker1/zeta";
 
 export const corsPlugin = createApp()
-  .onRequest(({ method, set }) => {
+  .onRequest(async ({ method, set }) => {
     set.headers["Access-Control-Allow-Origin"] = "*";
     set.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
     set.headers["Access-Control-Allow-Headers"] = "*";
     if (method === "OPTIONS") {
       set.status = 204;
+      return new Response(undefined, set);
     }
   })
   .export();
